470b2bc44ba033957432fae9d2895e8fdb980c0f
gongqiuwei <gongqiuwei@chendaimingdemacbook-pro.local>
2016-04-16 17:16:22 +0800

    登录界面: 登录框与注册框的切换

--- 仿百思不得姐/仿百思不得姐/Classes/FriendTrends/Controller/GWLoginRegisterViewController.m
+++ 仿百思不得姐/仿百思不得姐/Classes/FriendTrends/Controller/GWLoginRegisterViewController.m
@@ -9,6 +9,8 @@
 #import "GWLoginRegisterViewController.h"
 
 @interface GWLoginRegisterViewController ()
+/** 登录框距离控制器view左边的间距 */
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *loginViewLeftMargin;
 
 @end
 
@@ -17,6 +19,23 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    
+}
+
+- (IBAction)loginRegisterClicked:(UIButton *)button
+{
+    button.selected = !button.selected;
+    [self.view endEditing:YES];
+    
+    if (button.selected) { // 进入注册框
+        self.loginViewLeftMargin.constant = -self.view.width;
+    } else { // 进入登录框
+        self.loginViewLeftMargin.constant = 0;
+    }
+    
+    [UIView animateWithDuration:0.2 delay:0 usingSpringWithDamping:0.5 initialSpringVelocity:0.44 options:UIViewAnimationOptionCurveLinear animations:^{
+        [self.view layoutIfNeeded];
+    } completion:nil];
     
 }
 
